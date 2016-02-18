b7e83711465b859da2e7961afe51e28fed2d8fd4
tom <tomklamer@msn.com>
2016-02-18 14:57:47 +0100

    tom start structure

---
+++ Math-App/Math_App/Solutions/ISolutions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math_App.NewFolder
+{
+    public interface ISolutions
+    {
+        List<Solution> getSolutions();
+        void addSolution(Solution s);
+    }
+
+    public class Multiply : ISolutions
+    {
+        private List<Solution> solutions = new List<Solution>();
+
+        public List<Solution> getSolutions()
+        {
+            return this.solutions;
+        }
+
+        public void addSolution(Solution s)
+        {
+
+        }
+    }
+
+    public class Devide : ISolutions
+    {
+        private List<Solution> solutions = new List<Solution>();
+
+        public List<Solution> getSolutions()
+        {
+            return this.solutions;
+        }
+
+        public void addSolution(Solution s)
+        {
+            this.solutions.Add(s);
+        }
+    }
+
+    public class Minus : ISolutions
+    {
+        private List<Solution> solutions = new List<Solution>();
+
+        public List<Solution> getSolutions()
+        {
+            return this.solutions;
+        }
+
+        public void addSolution(Solution s)
+        {
+            this.solutions.Add(s);
+        }
+    }
+
+    public class Fracture : ISolutions
+    {
+        private List<Solution> solutions = new List<Solution>();
+
+        public List<Solution> getSolutions()
+        {
+            return this.solutions;
+        }
+
+        public void addSolution(Solution s)
+        {
+            this.solutions.Add(s);
+        }
+    }
+}
+
+}
