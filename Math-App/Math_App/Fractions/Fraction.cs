22206c321ab7bd87fdb8f5659536a128d61fade3
bartiz12 <bartekzwada@gmail.com>
2016-03-23 15:22:15 +0100

    Addition of fraction class.

--- Math-App/Math_App/Fractions/Fraction.cs
+++ Math-App/Math_App/Fractions/Fraction.cs
@@ -8,38 +8,65 @@ namespace Math_App.Fractions
 {
 	class Fraction
 	{
-		public string numerator;
-		public string denominator;
+		public float numerator = 0;
+		public float denominator = 0;
+		public float numeratorProper = 0;
+		public float unit = 0;
 
+		float value = 0;
 
-
-		public void setNumerator(string input)
+		//Convert input string to variables in class
+		public void setFraction(string input)
 		{
-			this.numerator = input;
+			List<string> result = input.Split(' ').ToList();
+
+			//Case of proper fraction like 2 and 1/5
+			if (result.Count() == 4)
+			{
+				this.unit = (float)Convert.ToDouble(result[0]);
+				this.numeratorProper = (float)Convert.ToDouble(result[1]);
+				this.denominator = (float)Convert.ToDouble(result[3]);
+
+				this.numerator = this.unit * this.denominator + this.numeratorProper;
+			}
+			else // case of fraction like 11 / 5
+			{
+				this.numerator = (float)Convert.ToDouble(result[0]);
+				this.denominator = (float)Convert.ToDouble(result[2]);
+				if (this.numerator > this.denominator)
+				{
+					this.updateUnit();
+					this.updateNumProper();
+				}
+				else
+				{
+					this.numeratorProper = this.numerator;
+				}
+			}
+
 		}
 
-		public void setNumerator(float input)
+		//update unit
+		void updateUnit()
 		{
-			this.numerator = input.ToString("G");
+			float temp = 0;
+			int count = 0;
+			temp = temp + this.denominator;
+
+			//count the amount of units in fraction
+			while (temp < this.numerator)
+			{
+				temp = temp + this.denominator;
+				count++;
+			}
+			this.unit = this.unit + (float)Convert.ToDouble(count);
 		}
 
-		public void setDenominator(string input)
+		//update numerator proper
+		void updateNumProper()
 		{
-			this.denominator = input;
+			this.numeratorProper = (this.numerator / this.denominator - this.unit) * this.denominator;
 		}
-
-		public void setDenominator(float input)
-		{
-			this.denominator = input.ToString("G");
-		}
-
-		public void setValue(string input)
-		{
-			//numerator = StringAnalyzer.getNumerator(input);
-			//numerator = StringAnalyzer.getDenominator(input);
-		}
-
-
 
 	}
 }
