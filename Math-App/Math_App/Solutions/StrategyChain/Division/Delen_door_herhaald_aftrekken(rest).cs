35479f654b9162e6b01b257b451ed8d82156ae1f
tom <tomklamer@msn.com>
2016-05-03 12:19:06 +0200

    tom

---
+++ Math-App/Math_App/Solutions/StrategyChain/Division/Delen_door_herhaald_aftrekken(rest).cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math_App.Solutions.StrategyChain.Division
+{
+    class Delen_door_herhaald_aftrekken_rest : ICheckStrategy
+    {
+        private ICheckStrategy nextInChain;
+        private bool use = false;
+        private int importance = 2;
+        public string title = "Delen door herhaald aftrekken (rest)";
+
+        public void DoAnalyze(string b, string c)
+        {
+            nextInChain.DoAnalyze(b, c);
+        }
+
+        public string ReturnTitle()
+        {
+            return this.title;
+        }
+
+        public ICheckStrategy ReturnStrat()
+        {
+            if (use)
+            {
+                return this;
+            }
+
+            return null;
+        }
+
+        public int ReturnImportance()
+        {
+            return this.importance;
+        }
+
+        public void setNextChain(ICheckStrategy nextChain)
+        {
+            this.nextInChain = nextChain;
+        }
+    }
+}
+
---
+++ Math-App/Math_App/Solutions/StrategyChain/Division/Delen_naar_analogie_met_nullen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math_App.Solutions.StrategyChain.Division
+{
+    class Delen_naar_analogie_met_nullen : ICheckStrategy
+    {
+        private ICheckStrategy nextInChain;
+        private bool use = false;
+        private int importance = 2;
+        public string title = "Delen naar analogie met nullen";
+
+        public void DoAnalyze(string b, string c)
+        {
+            nextInChain.DoAnalyze(b, c);
+        }
+
+        public string ReturnTitle()
+        {
+            return this.title;
+        }
+
+        public ICheckStrategy ReturnStrat()
+        {
+            if (use)
+            {
+                return this;
+            }
+
+            return null;
+        }
+
+        public int ReturnImportance()
+        {
+            return this.importance;
+        }
+
+        public void setNextChain(ICheckStrategy nextChain)
+        {
+            this.nextInChain = nextChain;
+        }
+    }
+}
+
