b3eaf3bb81c925608515ed84ecbbde6a7b6e57e9
mhulsie <maartenhulsman95@gmail.com>
2016-05-11 12:17:01 +0200

    aftrekken algoritmisch

---
+++ Math-App/Math_App/Solutions/StrategyChain/Substraction/Aftrekken_algoritmisch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math_App.Solutions.StrategyChain.Substraction
+{
+    public class Aftrekken_algoritmisch : ICheckStrategy
+    {
+        private ICheckStrategy nextInChain;
+        private bool use = false;
+        private int importance = 6;
+        public string title = "Aftrekken algoritmisch (cijferen)";
+
+        public void DoAnalyze(string b, string c)
+        {
+            this.use = true;
+
+            if (nextInChain != null)
+            {
+                nextInChain.DoAnalyze(b, c);
+            }
+        }
+
+        public string ReturnTitle()
+        {
+            return this.title;
+        }
+
+        public ICheckStrategy ReturnStrat()
+        {
+            if (use)
+            {
+                return this;
+            }
+
+            return null;
+        }
+
+        public int ReturnImportance()
+        {
+            return this.importance;
+        }
+
+        public void setNextChain(ICheckStrategy nextChain)
+        {
+            this.nextInChain = nextChain;
+        }
+    }
+}
